31bbd2320fc36bbc2fb8d4359554eea44bcc1c43
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2022-08-24 17:18:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-tools=(curl figlet)
+tools=(curl figlet wpscan)
 
 echo " "
 echo "$(tput setaf 3)[+] Instalaremos: curl, figlet y wpuserenum (en su sistema)."
